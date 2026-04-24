0d167f173c0cf5595c1b125302f7c2ee8cd79aae
Seu Nome <seuemail@gmail.com>
2026-04-24 10:11:25 -0300

    feat: Coloca o Lombook na classe para abstracao

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,11 +2,15 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cadastro_de_ninjas")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaModel {
 // faltou o modificador private nos atributos
     @Id
