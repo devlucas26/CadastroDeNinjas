0e385a8d17718ffa45e7b921c1690aafa0efceb6
Seu Nome <seuemail@gmail.com>
2026-05-01 13:06:02 -0300

    fix: Altera o nome das rotas do controller dos ninjas

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,15 +1,29 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping
+@RequestMapping("/missoes")
 @RestController
 public class MissoesController {
 
-    @GetMapping("/missoes")
-    public String missao(){
-        return "rota de missao";
+    @GetMapping("/listar")
+    public String listarTodos() {
+        return "listar todos os dados";
+    }
+    @PostMapping("/criar")
+    public String cadastrar(){
+        return "cadastro realizado com sucesso";
+    }
+    @PutMapping("/atualizar/id")
+    public String atualizar(){
+        return "atualização realizada com sucesso";
+    }
+    @DeleteMapping("/deletar/id")
+    public String deletar(){
+        return "deletado com sucesso";
+    }
+    @GetMapping("/listar/id")
+    public String listarId(){
+        return "procura missoes por id";
     }
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -3,38 +3,27 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninja")
 public class NinjaController {
 
-    @GetMapping("/boasvindas")
-    public String mensagem(){
-        return "endpoint do NinjaController";
+    @GetMapping("/listar")
+    public String listarTodos(){
+        return "listar todos os ninjas";
     }
-    // exemplo: esqueleto dos endpoints
-
-    // creat
     @PostMapping("/criar")
-    public String cadastra(){
-        return "ninja cadastrado";
+    public String cadastrarNinja(){
+        return "cadastro realizado com sucesso";
     }
-    // read - lista todos
-    @GetMapping("/todos")
-    public String listaTodos(){
-        return "lista todos os ninjas";
-    }
-    // read - procura pelo id
-    @GetMapping("/listaporid")
-    public String listaPorId(){
-        return "lista o ninja por id";
-    }
-    // update
-    @PutMapping("/atualiza")
+    @PutMapping("/atualizar/id")
     public String atualizarNinja(){
-        return "atualiza ninja por id";
+        return "atualização realizado com sucesso";
     }
-    // delete
-    @DeleteMapping("/deletar")
+    @GetMapping("/listar/id")
+    public String procuraPorId(){
+        return "procura ninja por id";
+    }
+    @DeleteMapping("/deletar/id")
     public String deletar(){
-        return "deletar por id";
+        return "deleção com sucesso";
     }
 }
