5ee5e83a1f230f38eb4debe940df4de9acf4aa85
Seu Nome <seuemail@gmail.com>
2026-05-01 12:00:58 -0300

    feat: atualiza o controller com mock de dados

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,32 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String mensagem(){
         return "endpoint do NinjaController";
+    }
+    // exemplo: esqueleto dos endpoints
+
+    // creat
+    @PostMapping("/criar")
+    public String cadastra(){
+        return "ninja cadastrado";
+    }
+    // read - lista todos
+    @GetMapping("/todos")
+    public String listaTodos(){
+        return "lista todos os ninjas";
+    }
+    // read - procura pelo id
+    @GetMapping("/listaporid")
+    public String listaPorId(){
+        return "lista o ninja por id";
+    }
+    // update
+    @PutMapping("/atualiza")
+    public String atualizarNinja(){
+        return "atualiza ninja por id";
+    }
+    // delete
+    @DeleteMapping("/deletar")
+    public String deletar(){
+        return "deletar por id";
     }
 }
